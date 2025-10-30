9ef09323bafa6ddbf5c380e5c13096a979fc4d54
jamessawle <980936+jamessawle@users.noreply.github.com>
2025-10-30 14:43:15 +0000

    dock: bump to v0.1.3

--- Formula/dock.rb
+++ Formula/dock.rb
@@ -1,8 +1,8 @@
 class Dock < Formula
   desc "macOS Dock Manager (YAML-driven, dry-run friendly)"
   homepage "https://github.com/jamessawle/dock"
-  url "https://github.com/jamessawle/dock/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "3349f27b9ce1122877c821f6431772bbb0d77da3f1d0f04e64e89c420e323fe3"
+  url "https://github.com/jamessawle/dock/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "956b9bfe20c9a74118bb381f349e6fca073a94f73e3c4a7ec70c3dbcfa8f189f"
   license "MIT"
 
   depends_on "dockutil"
