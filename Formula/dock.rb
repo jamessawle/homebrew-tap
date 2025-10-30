98de05c363fc920160b6c7666a8e57347168b335
jamessawle <980936+jamessawle@users.noreply.github.com>
2025-10-30 15:57:41 +0000

    dock: bump to v0.1.4

--- Formula/dock.rb
+++ Formula/dock.rb
@@ -1,8 +1,8 @@
 class Dock < Formula
   desc "macOS Dock Manager (YAML-driven, dry-run friendly)"
   homepage "https://github.com/jamessawle/dock"
-  url "https://github.com/jamessawle/dock/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "956b9bfe20c9a74118bb381f349e6fca073a94f73e3c4a7ec70c3dbcfa8f189f"
+  url "https://github.com/jamessawle/dock/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "37e92eccc1db08e78e4c11260d23c86c76787fee633d231a3c97b993656f43ad"
   license "MIT"
 
   depends_on "dockutil"
@@ -12,6 +12,8 @@ class Dock < Formula
   def install
     libexec.install "dock", "lib"
     bin.install_symlink libexec/"dock"
+    bash_completion.install "completions/dock.bash" => "dock"
+    zsh_completion.install "completions/_dock"
   end
 
   test do
