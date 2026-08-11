42f6fff309c2fd0509f0c457db23eac19de4785f
James Sawle <jamessawle@hotmail.com>
2026-08-11 10:20:06 +0000

    Fix sbxflow cask stanza grouping

--- Casks/sbxflow.rb
+++ Casks/sbxflow.rb
@@ -3,11 +3,12 @@ cask "sbxflow" do
 
   on_arm do
     sha256 "612cfdfe12321bb9899ae56d06d24bdf7f3d73bc36d47cd95989db92190dd952"
+
     url "https://github.com/jamessawle/sbxflow/releases/download/v#{version}/sbxflow_#{version}_darwin_arm64.tar.gz"
   end
-
   on_intel do
     sha256 "79bf68bebd5922e9c05e2fcd3b3bcf1f5a6f66a922d1a86b96580d4d494712ff"
+
     url "https://github.com/jamessawle/sbxflow/releases/download/v#{version}/sbxflow_#{version}_darwin_amd64.tar.gz"
   end
 
